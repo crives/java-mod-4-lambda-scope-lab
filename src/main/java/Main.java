515398d4c249a088afe0afde68b793c1a3deaa93
Caroline Rives <car.rives@gmail.com>
2022-07-19 14:41:21 -0400

    Completed lambda scope lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,22 @@
 import java.util.function.UnaryOperator;
 
+/*
+    Fix the modifiers of the PREFIX and SUFFIX variables
+    so they can be used in the operator lambda expression.
+    Fix the lambda expression so that:
+    str doesn’t have any whitespace.
+    The PREFIX and SUFFIX values are added to the string.
+ */
 public class Main {
-    String PREFIX = "__prefix__";
-    String SUFFIX = "__suffix__";
+    static String PREFIX = "__prefix__";
+    static String SUFFIX = "__suffix__";
 	
 	// fix the operator code
-    public static UnaryOperator<String> operator = str -> str;
+    public static UnaryOperator<String> operator = str -> str.replaceAll(" ", "");
 
     public static void main(String[] args) {
+
         String res = operator.apply("  hello"); 
-        System.out.println(res); // __prefix__hello__suffix__
+        System.out.println(PREFIX + res + SUFFIX); // __prefix__hello__suffix__
     }
 }
